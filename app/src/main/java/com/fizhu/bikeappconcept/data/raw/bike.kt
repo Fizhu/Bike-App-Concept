9ff236f7d490a1bfa2d4179800c4e9466c56750a
Hafizh Anbiya <hvyz.anbiya@gmail.com>
2020-07-10 17:20:16 +0700

    update bike

---
+++ app/src/main/java/com/fizhu/bikeappconcept/data/raw/bike.kt
@@ -0,0 +1,12 @@
+package com.fizhu.bikeappconcept.data.raw
+
+/**
+ * Created by fizhu on 10,July,2020
+ * https://github.com/Fizhu
+ */
+object bike {
+    private val imgRoadbike = "https://raw.githubusercontent.com/Fizhu/Bike-App-Concept/master/images/roadbike.png"
+    private val imgMtb = "https://raw.githubusercontent.com/Fizhu/Bike-App-Concept/master/images/mtb.png"
+    private val imgBmx = "https://raw.githubusercontent.com/Fizhu/Bike-App-Concept/master/images/bmx2.png"
+
+}
